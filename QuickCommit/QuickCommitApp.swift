92e4316de69c19979395a3498e54cd24692fe5de
Brave New Design LLC <bravenewdesign@users.noreply.github.com>
2026-08-03 15:18:30 -0400

    Initial Commit

---
+++ QuickCommit/QuickCommitApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCommitApp.swift
+//  QuickCommit
+//
+//  Created by Shawn Crawford on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickCommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickCommitTests/QuickCommitTests.swift
@@ -0,0 +1,19 @@
+//
+//  QuickCommitTests.swift
+//  QuickCommitTests
+//
+//  Created by Shawn Crawford on 8/3/26.
+//
+
+import Testing
+@testable import QuickCommit
+
+struct QuickCommitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
